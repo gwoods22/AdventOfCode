4dd208ae09031d6a10d3451d869b0e27a744ee30
Graeme Woods <graemewoods202@gmail.com>
2023-12-08 17:42:10 -0500

    remove incomplete files

--- 2023/9.ts
+++
@@ -1,9 +0,0 @@
-import { strData } from './load.js';
-
-const startTime = performance.now()
-
-
-console.log('Part A is', );
-console.log('Part B is', );
-
-console.log('Took:', ((performance.now() - startTime)/1000).toFixed(4), 'seconds to run');
